71fb1956fe68a94545afde2ecfe4e7006bd73043
emilienbai <emilien.bai@insa-lyon.fr>
2015-03-13 17:35:52 +0100

    Ajout de la classe conseiller

---
+++ src/b3425/Conseiller.java
@@ -0,0 +1,43 @@
+package b3425;
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+/**
+*
+*@author ebai
+*/
+
+@Entity
+public class Conseiller{
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int id;
+private String nom;
+private String prenom;
+private String mail;
+
+public Conseiller(){
+
+}
+
+public Conseiller(String nom, String prenom, String mail){
+	this.nom=nom;
+	this.prenom=prenom;
+	this.mail=mail;
+}
+
+public int getId(){
+	return this.id;
+}
+
+public String getNom(){
+	return this.nom;
+}
+
+public void setNom(String nom){
+	this.nom=nom;
+}
+
+public 
+}
